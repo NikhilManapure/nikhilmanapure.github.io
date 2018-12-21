f0f1e8335baaee5242723bbb2ba206e7e8621759
Nikhil <nikhil.manapure@wealthtrust.in>
2018-12-21 17:53:33 +0530

    experimenting a bit

--- src/app/components/about-page/about-page.component.ts
+++ src/app/components/about-page/about-page.component.ts
@@ -10,6 +10,20 @@ export class AboutPageComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    this.typeWriter()
   }
 
+  i = 0;
+  txt = 'Hii, nice to meet you. My name is Nikhil Manapure. I am an iOS developer.';
+  speed = 50;
+
+  typeWriter() {
+    if (this.i < this.txt.length) {
+      document.getElementById("intro").innerHTML += this.txt.charAt(this.i);
+      this.i++;
+      setTimeout(() => {
+        this.typeWriter();
+      }, this.speed);
+    }
+  }
 }
